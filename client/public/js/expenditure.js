03bb6ff790051aa2a07414e77dcd03d1c873c7b2
Kieran-Antonio <arul.kieran@gmail.com>
2022-07-25 11:18:04 +0100

    added data viz plan

--- client/public/js/expenditure.js
+++ client/public/js/expenditure.js
@@ -2,6 +2,15 @@ window.onload = () => {
 
     fetch("/getChartData")
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+
+            // CHART CODE GOES HERE
+            // data variable above is in an array and is what we need to process --> it will looks something like:
+            // [{item: x, cost: y, category: z...}, {item: x, cost: y, category: z...}]
+            // Our end goal is to process the data and create an array like [a, b, c, d], where a, b, c, d represent
+            // the total expenditure of each category respectively
+
+        })
+        .catch(err => console.log(err));
 
 }
