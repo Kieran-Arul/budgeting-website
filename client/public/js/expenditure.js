a57f554a1c3ebb7380cc26b99cd57c3f67dfdb22
James-Sunter-Storey <s1976220@ed.ac.uk>
2022-07-25 11:56:23 +0100

    Chart Base Info JS

--- client/public/js/expenditure.js
+++ client/public/js/expenditure.js
@@ -4,7 +4,41 @@ window.onload = () => {
         .then(response => response.json())
         .then(data => {
 
-            // CHART CODE GOES HERE
+            anychart.onDocumentReady(function() {
+
+                anychart.theme(anychart.themes.darkEarth);
+
+                // set the data
+                var data = {
+                  header: ["Category", "Total"],
+                  rows: [
+                    ["Groceries", ],
+                    ["Rent", ],
+                    ["Travel", ],
+                    ["Entertainment", ],
+                    ["Bills", ],
+                    ["Shopping", ],
+                    ["Eating Out", ],
+                    ["Cash", ],
+                    ["Payments", ],
+                    ["Direct Debits", ],
+                    ["Other", ]
+                ]};
+
+                // create the chart
+               var chart = anychart.bar();
+
+                // add data
+                chart.data(data);
+
+                // set the chart title
+                chart.title("The deadliest earthquakes in the XXth century");
+
+              // draw
+              chart.container("container");
+              chart.draw();
+            });
+
             // data variable above is in an array and is what we need to process --> it will looks something like:
             // [{item: x, cost: y, category: z...}, {item: x, cost: y, category: z...}]
             // Our end goal is to process the data and create an array like [a, b, c, d], where a, b, c, d represent
